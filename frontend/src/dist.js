40656b4a5eebd5022e75703997d7938b99f36903
Parsa Rostami <parsa84rostami@gmail.com>
2023-08-09 17:11:31 +0330

    Delete frontend directory

--- frontend/src/dist.js
+++
@@ -1,7 +0,0 @@
-const SeperatePrice = (price) => {
-    const numberFormatter = Intl.NumberFormat('en-US');
-    const formatted = numberFormatter.format(price);
-    return(formatted)
-}
-
-export {SeperatePrice}
